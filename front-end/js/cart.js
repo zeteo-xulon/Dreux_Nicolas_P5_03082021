643d817cb05dfd33ebbb618f96282499ce06038c
zeteo_xulon <n.dreux@live.fr>
2021-09-29 08:56:25 +0200

    Adding code to send to local storage a product that have been selected, and its quantity // create a Javascript file linked with the cart.html and adding on it the code to exctract from local storage the product // will be upgraded for more products

---
+++ front-end/js/cart.js
@@ -0,0 +1,47 @@
+//=============================
+//      VARIABLE
+//=============================
+const url = "http://localhost:3000/api/teddies";
+const cartArticleContainer = document.getElementById("cartArticle");
+
+let incartProduct = JSON.parse(localStorage.getItem("productItem"));
+let productId = incartProduct.id;
+
+//=============================
+//      SCENARIO
+//=============================
+function display() {
+	fetch(url + "/" + productId)
+		.then((res) => res.json())
+		.then((e) => {
+			// (1) | Injecting HTML here.
+			cartArticleContainer.innerHTML = injectTeddiesInCartList(e);
+		});
+}
+display();
+
+//=============================
+//      FUNCTION
+//=============================
+function injectTeddiesInCartList(param) {
+	return `
+  <a href="./front-end/pages/product.html?_id=${param._id}">
+  <img class="cart__product__img" src="${param.imageUrl}">
+  </a>
+
+  <div class="cart__product__box">
+    <h2 class="cart__product__name">${param.name}</h2>
+    <h3 class="cart__product__id">${param._id}</h3>
+  </div>
+
+  <p class="cart__product__color">${incartProduct.color}</p>
+
+  <div class="cart__product__number__box">
+    <input type="number" name="product__quantity" id="cartProductQuantity" min="0" max="9" value='${
+			incartProduct.quantity
+		}'>
+  </div>
+
+  <p class="cart__product__price">${parseInt(param.price) / 100}</p>
+  `;
+}
--- front-end/js/product.js
+++ front-end/js/product.js
@@ -1,63 +1,66 @@
 //==========================================
-// Variables
+// Variables List
+//==========================================
+const url = "http://localhost:3000/api/teddies";
 
+//Pointer
+const mainProduct = document.querySelector("main");
+
+// Get the id from the url
 let urlParameter = new URLSearchParams(window.location.search);
 let id = urlParameter.get("_id");
-console.log(urlParameter);
 
-const fetchAdress = "http://localhost:3000/api/teddies" + "/" + id;
-const mainProduct = document.querySelector('main');
+// Use for stack the color option data.
 let optionColor = "";
-
-
-//=====================================
-//Console log for checking data
-console.log(fetchAdress);
-console.log(id);
-console.log(mainProduct);
-
+let getColor = "";
 
 //=====================================
-//Display the teddie with it's information and specificities. 
+//        SCENARIO
+//=====================================
 
-const display = () => {
-	fetch(fetchAdress)
+//Display the teddie with it's information and specificities.
+function fetchData() {
+	fetch(url + "/" + id)
 		.then((res) => res.json())
 		.then((product) => {
-      console.log(product);
+			// (1) | playing the loop to get the color before injecting HTML.
+			loopColor(product);
 
-      //playing the loop to get the color before injecting HTML. | (1)
-      loopColor(product);
+			// (2) | Injecting HTML here.
+			mainProduct.innerHTML = teddieDisplayInfo(product);
 
-      //Injecting HTML here. | (2)
-      mainProduct.innerHTML = teddieDisplayInfo(product);
-    })
+			// (2) | Initialize the listener to store data in the LocalStorage for the cart page.
+			submit();
+		})
 		.catch((err) => {
 			console.log(err);
-			mainProduct.innerHTML = `
-    <h2>Veuillez réessayer utltérieumenent, une erreur s'est produite.</h2>
-    `;
+			//Display an information for the user that teddies could not load.
+			displayErrorMessage();
 		});
-};
+}
+fetchData();
 
-//=====================================================
+/*=====================================================
+ *       FUNCTION LIST
+ *====================================================*/
+
 // (1) | A loop for to go throught all the color in the list and add it.
-const loopColor = (param) => {
-  
-  for(let color of param.colors) {
-    optionColor += `<option value="${color}">${color}</option>`;
-  };
+function loopColor(param) {
+	for (let color of param.colors) {
+		optionColor += `<option value="${color}">${color}</option>`;
+	}
+}
 
-};
-
-//=====================================================
-// (2) | Display all information of the teddie in the card
-const teddieDisplayInfo = (product) => {
-      return `
+//----------------
+// (2) | Display all information of the teddie in the html main__product
+function teddieDisplayInfo(product) {
+	return `
       <section class="product-card">
 
       <div class="product__img-box" >
-        <img class="product__img" src="${product.imageUrl}" alt="Image du nounours ${product.name}" />
+        <img class="product__img" src="${
+					product.imageUrl
+				}" alt="Image du nounours ${product.name}" />
       </div>
 
       <article class="product__informations">
@@ -65,27 +68,51 @@ const teddieDisplayInfo = (product) => {
           <h2 class="product__name">${product.name}</h2>
           <p class="product__id">Référence : ${product._id}</p>
           <label for="option__color">Choisissez votre couleur de peluche</label>
-              <select id="option__color">
+              <select id="optionColor">
                 ${optionColor}
                 </select>
-          <input type="number" name="product__quantity" id="productQuantity" min="0" max="9">
+          <input type="number" name="product__quantity" id="productQuantity" min="0" max="9" value="1" />
         
           <aside class="product__description">
             <h3 class="product__title">Description</h3>
-            <p class="product__description__paragraph">${product.description}</p>
+            <p class="product__description__paragraph">${
+							product.description
+						}</p>
           </aside>
 
           <div class="center-box">
-            <p class="product__price">${parseInt(product.price) /100} €</p>
-            <button class="btn">Ajouter au panier</button>
+            <p class="product__price">${parseInt(product.price) / 100} €</p>
+            <button class="btn" id="productSubmit">Ajouter au panier</button>
           </div>
 
       </article>
     
     </section>
       `;
+}
+
+//----------------
+// will display a H2 information message for the user if fetch catch an error.
+const displayErrorMessage = () => {
+	mainProduct.innerHTML = `
+    <h2>Veuillez réessayer utltérieumenent, une erreur s'est produite.</h2>
+    `;
 };
+
+async function submit() {
+	const productColor = document.getElementById("optionColor").value;
+	const productQuantity = document.getElementById("productQuantity").value;
+	const submitButton = document.getElementById("productSubmit");
+	//on click, it will collect all the data
+	submitButton.onclick = function (data) {
+		let productItem = {
+			id: id,
+			color: productColor,
+			quantity: productQuantity,
+		};
+		let productSerialized = JSON.stringify(productItem);
+		localStorage.setItem("productItem", productSerialized);
+	};
+}
+
 //=======================================
-
-display();
-
