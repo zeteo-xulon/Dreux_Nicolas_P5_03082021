79f1ca530e47743c008d1892b6d8af5ed52e56f9
zeteo_xulon <n.dreux@live.fr>
2021-10-01 17:05:33 +0200

    Creating a container in HTML to display a text throught JS // Adding conditionnary to verify whats in the localStorage, will display a message if there is nothing. // adding conditionnary to check the quantity of the products. Will display a message if there is more than 9.

--- front-end/js/cart.js
+++ front-end/js/cart.js
@@ -3,9 +3,13 @@
 //=============================
 const url = "http://localhost:3000/api/teddies";
 const cartArticleContainer = document.getElementById("cartArticle");
+const items = localStorage.productItem;
+const itemQuantity = parseInt(items.quantity);
+const messageBox = document.getElementById("display__message");
 
 let incartProduct = JSON.parse(localStorage.getItem("productItem"));
 let productId = incartProduct.id;
+let productInCart = false;
 
 //=============================
 //      SCENARIO
@@ -14,8 +18,11 @@ function display() {
 	fetch(url + "/" + productId)
 		.then((res) => res.json())
 		.then((e) => {
-			// (1) | Injecting HTML here.
-			cartArticleContainer.innerHTML = injectTeddiesInCartList(e);
+      // (1) check the quantity of product in the cart
+      LocalStorageQuantityCheck();
+
+			// (2) | Injecting HTML here.
+			cartInjection(e);
 		});
 }
 display();
@@ -23,6 +30,55 @@ display();
 //=============================
 //      FUNCTION
 //=============================
+
+/*
+ *Fonction qui va inspecter ce qu'il y a dans le LocalStorage
+ *
+ * Si il y a quelque chose, alors elle va afficher les produits
+ *   Si la quantité est > 9 alors quantité du produit === 9
+ *   Ajout d'un texte "La quantité maximal est atteinte."
+ *
+ *Si il n'y a rien, alors afficher un message
+ * "votre panier est vide."
+ *
+ */
+
+const LocalStorageQuantityCheck = (param) => {
+	if (items.length > 0) {
+		if (itemQuantity > 9) {
+			itemQuantity = 9;
+			displayQuantityWarningMessage();
+      return productInCart = true;
+		}
+	}
+  if(items.length === 0){
+    return productInCart = false;
+  }
+  return productInCart = true;
+};
+
+/* Fonction conditionnel qui vérifie que le panier à quelque chose
+* Si c'est vrai
+*   lancer la fonction d'injection de HTML.
+*/
+
+function cartInjection(param) {
+  if (productInCart === true) {
+    htmlInjector(param);
+  }
+  if (productInCart === false){
+    displayEmptyCartMessage();
+  }
+}
+
+
+
+//-----------------------------------------
+
+const htmlInjector = (param) => {
+  cartArticleContainer.innerHTML = injectTeddiesInCartList(param);
+}
+
 function injectTeddiesInCartList(param) {
 	return `
   <a class="cart__link__product" href="./front-end/pages/product.html?_id=${
@@ -50,4 +106,12 @@ function injectTeddiesInCartList(param) {
 
   
   `;
+}
+
+const displayQuantityWarningMessage = () => {
+	messageBox.innerHTML = `<p class="message__text">La quantité maximal est de 9.</p>`;
+};
+
+const displayEmptyCartMessage = () => {
+  cartArticleContainer.innerHTML = `<p class="empty__cart">Le panier est vide.</p>`;
 }
